7816affe0c4330cfa4abc9724004153b40a9b1ab
Davis Cruz <daviccc98@gmail.com>
2023-11-18 16:54:40 -0600

    Crear el proyecto

---
+++ OpenIaProyecto/OpenIaProyecto/OpenIaProyectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenIaProyectoApp.swift
+//  OpenIaProyecto
+//
+//  Created by Davis Cruz on 11/18/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenIaProyectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
